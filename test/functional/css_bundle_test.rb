e93ef1ce3729091f53a366f921d303d0fff40005
timcharper <timcharper@1db77ec0-6337-0410-9320-454da9aca44f>
2007-10-31 04:03:05 +0000

    js minimizer some refactoring, code cleanup
    git-svn-id: http://bundle-fu.googlecode.com/svn/trunk@50 1db77ec0-6337-0410-9320-454da9aca44f

--- test/functional/css_bundle_test.rb
+++ test/functional/css_bundle_test.rb
@@ -2,15 +2,18 @@ require File.join(File.dirname(__FILE__), '../test_helper.rb')
 
 class CSSBundleTest < Test::Unit::TestCase
   def test__rewrite_relative_path__should_rewrite
-    assert_equal("/images/spinner.gif", BundleFu.rewrite_relative_path("/stylesheets/active_scaffold/default/stylesheet.css", "../../../images/spinner.gif"))
-    assert_equal("/images/spinner.gif", BundleFu.rewrite_relative_path("/stylesheets/active_scaffold/default/stylesheet.css", "../../../images/./../images/goober/../spinner.gif"))
-    assert_equal("/images/spinner.gif", BundleFu.rewrite_relative_path("stylesheets/active_scaffold/default/./stylesheet.css", "../../../images/spinner.gif"))
-    assert_equal("/stylesheets/image.gif", BundleFu.rewrite_relative_path("stylesheets/main.css", "image.gif"))
-    assert_equal("/stylesheets/image.gif", BundleFu.rewrite_relative_path("/stylesheets////default/main.css", "..//image.gif"))
-    assert_equal("/images/image.gif", BundleFu.rewrite_relative_path("/stylesheets/default/main.css", "/images/image.gif"))
+    assert_equal("/images/spinner.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("/stylesheets/active_scaffold/default/stylesheet.css", "../../../images/spinner.gif"))
+    assert_equal("/images/spinner.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("/stylesheets/active_scaffold/default/stylesheet.css", "../../../images/./../images/goober/../spinner.gif"))
+    assert_equal("/images/spinner.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("stylesheets/active_scaffold/default/./stylesheet.css", "../../../images/spinner.gif"))
+    assert_equal("/stylesheets/image.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("stylesheets/main.css", "image.gif"))
+    assert_equal("/stylesheets/image.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("stylesheets/main.css", "'image.gif'"))
+    assert_equal("/stylesheets/image.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("stylesheets/main.css", " image.gif "))
+    assert_equal("/stylesheets/image.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("/stylesheets////default/main.css", "..//image.gif"))
+    assert_equal("/images/image.gif", BundleFu::CSSUrlRewriter.rewrite_relative_path("/stylesheets/default/main.css", "/images/image.gif"))
   end
   
   def test__bundle_css_file__should_rewrite_relatiave_path
+#    dbg
     bundled_css = BundleFu.bundle_css_files(["/stylesheets/css_3.css"])
 #    puts bundled_css
     assert_match("background-image: url(/images/background.gif)", bundled_css)
